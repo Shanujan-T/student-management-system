521e570d73a9f69d0030644bde4dad1fc38c8467
Shanujan <shanushanujan234@gmail.com>
2026-06-08 23:19:56 +0530

    __init__.py , requirements.txt updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from app.config import Config
 from sqlalchemy import text
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     
+    app.config.from_object(Config)
+
     db.init_app(app)
 
     from app.routes.student_route import student_bp
@@ -20,4 +25,4 @@ def create_app():
         print("SUCCESS: Database Connected Successfully")
         db.create_all()
 
-    return app   
+    return app 
