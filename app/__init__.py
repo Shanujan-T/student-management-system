2a2daf4b8fa4ce49d85f5f61b121bba66e052fd7
Shanujan <shanushanujan234@gmail.com>
2026-06-08 22:36:15 +0530

    course model created

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from sqlalchemy import text
 
-
 db = SQLAlchemy()
 
 def create_app():
--- app/models/student_model.py
+++ app/models/student_model.py
@@ -2,7 +2,7 @@ from app import db
 
 class Student(db.Model):
     __tablename__="students"
-    student_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    student_id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
