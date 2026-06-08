8c7b2a9532ac59561cc47ef030215520fb41867c
Shanujan <shanushanujan234@gmail.com>
2026-06-08 21:36:26 +0530

    student model created

---
+++ app/models/student_model.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Student(db.Model):
+    __tablename__="students"
+    student_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_of_birth = db.Column(db.Date, nullable=False)
