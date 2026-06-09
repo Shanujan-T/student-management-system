b03f7073c12f315101306a3ce0134d95737f6e90
Shanujan <shanushanujan234@gmail.com>
2026-06-09 06:12:56 +0530

    course model updated

--- app/models/course_model.py
+++ app/models/course_model.py
@@ -3,7 +3,7 @@ from app import db
 class Course(db.Model):
     __tablename__="courses"
     course_id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-    course_code = db.Column(db.String(20), nullable=False)
+    course_code = db.Column(db.String(20), nullable=False, unique=True)
     course_name = db.Column(db.String(100), nullable=False)
-    credits = db.Column(db.Integer, nullable=False, unique=True)
+    credits = db.Column(db.Integer, nullable=False)
     lecturer_id = db.Column(db.Integer, db.ForeignKey("lecturers.lecturer_id"), nullable=False)
