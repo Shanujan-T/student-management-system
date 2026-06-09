4075f564c55a9ee9c9ee96c6c9bfa4ec02ea3a3c
Shanujan <shanushanujan234@gmail.com>
2026-06-09 05:41:34 +0530

    entrollment model updated

--- app/models/enrollment_model.py
+++ app/models/enrollment_model.py
@@ -5,5 +5,5 @@ class Enrollment(db.Model):
     enrollment_id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     student_id = db.Column(db.Integer, db.ForeignKey("students.student_id"), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey("courses.course_id"), nullable=False)
-    enrollment_date = db.Column(db.Date, nullable=False, unique=True)
+    enrollment_date = db.Column(db.Date, nullable=False)
     status = db.Column(db.String(20), nullable=False)
